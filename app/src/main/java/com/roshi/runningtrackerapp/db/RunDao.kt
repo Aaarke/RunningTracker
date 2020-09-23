0c378938c2a6b2244ec9e43a4c59076201907ce6
Roshan kumar <roshankumarroshi@gmail.com>
2020-09-23 23:06:02 +0530

    initial commit

---
+++ app/src/main/java/com/roshi/runningtrackerapp/db/RunDao.kt
@@ -0,0 +1,41 @@
+package com.roshi.runningtrackerapp.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface RunDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRunData(runData: RunData)
+
+    @Delete
+    suspend fun deleteRun(runData: RunData)
+
+    @Query("SELECT * FROM running_table ORDER By timeStamp DESC")
+    fun getAllRunDataSortedByDate(): LiveData<List<RunData>>
+
+    @Query("SELECT * FROM running_table ORDER By timeInmilis DESC")
+    fun getAllRunDataSortedByMiliSecond(): LiveData<List<RunData>>
+
+    @Query("SELECT * FROM running_table ORDER By caloriesBurned DESC")
+    fun getAllRunDataSortedByCalorie(): LiveData<List<RunData>>
+
+    @Query("SELECT * FROM running_table ORDER By averageSpeed DESC")
+    fun getAllRunDataSortedByAverageSpeed(): LiveData<List<RunData>>
+
+    @Query("SELECT * FROM running_table ORDER By distanceInMeters DESC")
+    fun getAllRunDataSortedByDistance(): LiveData<List<RunData>>
+
+    @Query("SELECT SUM(timeInmilis) FROM running_table")
+    fun getTotalTimeInMiliSecond():LiveData<Long>
+
+    @Query("SELECT SUM(caloriesBurned) FROM running_table")
+    fun getTotalCalorieBurned():LiveData<Int>
+
+    @Query("SELECT SUM(distanceInMeters) FROM running_table")
+    fun getTotalDistanceInMeters():LiveData<Long>
+
+    @Query("SELECT AVG(averageSpeed) FROM running_table")
+    fun getTotalAverageSpeed():LiveData<Float>
+
+}
---
+++ app/src/main/java/com/roshi/runningtrackerapp/db/RunData.kt
@@ -0,0 +1,17 @@
+package com.roshi.runningtrackerapp.db
+
+import android.graphics.Bitmap
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "running_table")
+class RunData(var image:Bitmap?=null,
+              var timeStamp:Long=0L,
+              var averageSpeed:Float,
+              var distanceInMeters:Int=0,
+              var timeInmilis:Long=0L,
+              var caloriesBurned:Int=0
+) {
+    @PrimaryKey(autoGenerate = true)
+    var id:Int?=null
+}
