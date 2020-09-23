f43c9670a23a625a4643d7156b0fa22243ebc148
Roshan kumar <roshankumarroshi@gmail.com>
2020-09-23 23:58:11 +0530

    database class added

---
+++ app/src/main/java/com/roshi/runningtrackerapp/db/RunningDataBase.kt
@@ -0,0 +1,12 @@
+package com.roshi.runningtrackerapp.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+
+@Database(entities = [RunData::class],version = 1)
+@TypeConverters(Converters::class)
+abstract class RunningDataBase: RoomDatabase() {
+    abstract fun getRunDao():RunDao
+}
